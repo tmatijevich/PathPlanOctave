a9fb0cf63ebf91c1e61bb8593452796a7712b130
Tyler Matijevich <tyler.matijevich@br-automation.com>
2020-05-15 11:22:23 -0500

    Test for two points

--- demo_Kin_GetVelProfPoint.m
+++ demo_Kin_GetVelProfPoint.m
@@ -3,13 +3,14 @@
 v = [0.0, 1.9, 0.75, 0.75, 1.2];
 t = [0.0, 0.25, 0.40, 0.50, 0.60];
 
-tplot = t(1):0.01:t(end); n = length(tplot);
+N = 5;
+tplot = t(1):0.01:t(N); n = length(tplot);
 xplot = zeros(n,1);
 vplot = zeros(n,1);
 aplot = zeros(n,1);
 
 for i = 1:n
-	[soln, valid] = Kin_GetVelProfPoint(0.0, t, v, 5, tplot(i));
+	[soln, valid] = Kin_GetVelProfPoint(0.0, t(1:N), v(1:N), N, tplot(i));
 	if valid
 		xplot(i) = soln.x;
 		vplot(i) = soln.v;
