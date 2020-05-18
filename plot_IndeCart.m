a57f90f4ea83cbfe940513150e80ab968bdcfc60
Tyler Matijevich <tyler.matijevich@br-automation.com>
2020-05-18 08:47:02 -0500

    Finish plot with pallet spacing

--- plot_IndeCart.m
+++ plot_IndeCart.m
@@ -52,8 +52,9 @@ et = [et, et(end) + it];
 ev = [ev, iv];
 
 
+tstep = 0.005;
 % Position & acceleration
-itplot = it(1):0.005:it(end); n = length(itplot);
+itplot = it(1):tstep:it(end); n = length(itplot);
 ixplot = zeros(1,n);
 iaplot = zeros(1,n);
 x0 = gPar.LoadPosition;
@@ -64,7 +65,7 @@ for i = 1:n
 		iaplot(i) = soln.a;
 	end
 end
-ftplot = f_t(1):0.001:f_t(end); n = length(ftplot);
+ftplot = (ceil(f_t(1) / tstep) * tstep):tstep:f_t(end); n = length(ftplot);
 fxplot = zeros(1,n);
 faplot = zeros(1,n);
 x0 = gPar.LoadPosition + gCalc.TotalIndexDistance + gCalc.RiseDistance;
@@ -75,7 +76,7 @@ for i = 1:n
 		faplot(i) = soln.a;
 	end
 end
-stplot = s_t(1):0.001:s_t(end); n = length(stplot);
+stplot = (ceil(s_t(1) / tstep) * tstep):tstep:s_t(end); n = length(stplot);
 sxplot = zeros(1,n);
 saplot = zeros(1,n);
 x0 = gPar.LoadPosition + gCalc.TotalIndexDistance + gCalc.RiseDistance;
@@ -86,7 +87,7 @@ for i = 1:n
 		saplot(i) = soln.a;
 	end
 end
-etplot = et(1):0.001:et(end); n = length(etplot);
+etplot = et(1):tstep:et(end); n = length(etplot);
 explot = zeros(1,n);
 eaplot = zeros(1,n);
 x0 = gPar.LoadPosition - (RouteVelocity ^ 2) / (2.0 * gCalc.ProfAcceleration) - (gCalc.PalletPeriod - RouteVelocity / gCalc.ProfAcceleration) * RouteVelocity;
@@ -98,23 +99,39 @@ for i = 1:n
 	end
 end
 
+dxplot1 = ixplot - explot(1:length(ixplot));
+dxplot2 = sxplot(1:(length(explot) - length(ixplot))) - explot((length(ixplot) + 1):end);
+
 % Set default line width
 set(groot, "defaultLineLineWidth", 1.75);
 
 hFig = figure(1, "name", "IndeCart"); set(hFig, "menubar", "none");
 cur = get(hFig, 'position');
-set(hFig, 'position', [cur(1), cur(2), 800, 800]);
-subplot(3,1,1); cla; hold on;
-plot(etplot, explot, "k", 'linewidth', 1.0);
-plot(itplot, ixplot, "b");
-plot(ftplot, fxplot, "r--");
-plot(stplot, sxplot, "g--");
+set(hFig, 'position', [cur(1), cur(2), 800, 1000]);
+subplot(4,1,1); cla; hold on;
+hPlte = plot(etplot, explot, "k", 'linewidth', 1.0);
+hPlti = plot(itplot, ixplot, "b");
+hPltf = plot(ftplot, fxplot, "r--");
+hPlts = plot(stplot, sxplot, "g--");
+hLeg = legend([hPlti, hPltf, hPlts, hPlte], {"Current Pallet", "Fastest", "Slowest", "Incoming Pallet"}, 'location', 'northeast');
 xlim([0.0, 1.6]);
 ylim([round(10 * (gPar.LoadPosition - 0.100)) / 10.0, round(10 * (gPar.DropPosition + gPar.DropOffset + 0.100)) / 10.0]);
+set(hLeg, 'fontsize', 12);
 set(gca, 'fontsize', 12);
 ylabel("Position [m]");
 
-subplot(3,1,2); cla; hold on;
+subplot(4,1,2); cla; hold on;
+hPlt1 = plot(itplot, dxplot1, "k", 'linewidth', 1.0);
+hPlt2 = plot(etplot((length(ixplot) + 1):end), dxplot2, "g--");
+hPltMin = plot([itplot(1) etplot(end)], [gCalc.MinPalletSpacing, gCalc.MinPalletSpacing], "m--");
+hLeg = legend([hPlt1, hPlt2, hPltMin], {"Current Spacing", "Slow Path Spacing", "Min Spacing"}, 'location', 'northeast');
+xlim([0.0, 1.6]);
+ylim([0.0 0.5]);
+set(hLeg, 'fontsize', 12);
+set(gca, 'fontsize', 12);
+ylabel("Pallet Spacing [m]");
+
+subplot(4,1,3); cla; hold on;
 hPlte = plot(et, ev, "k", 'linewidth', 1.0);
 hPlti = plot(it, iv, "b");
 hPltf = plot(f_t, f_v, "r--");
@@ -125,11 +142,13 @@ set(hLeg, 'fontsize', 12);
 set(gca, 'fontsize', 12);
 ylabel("Velocity [m/s]");
 
-subplot(3,1,3); cla; hold on;
-plot(itplot, iaplot, "b");
-plot(ftplot, faplot, "r--");
-plot(stplot, saplot, "g--");
+subplot(4,1,4); cla; hold on;
+hPlti = plot(itplot, iaplot, "b");
+hPltf = plot(ftplot, faplot, "r--");
+hPlts = plot(stplot, saplot, "g--");
+hLeg = legend([hPlti, hPltf, hPlts], {"Current Pallet", "Fastest", "Slowest"}, 'location', 'northeast');
 xlim([0.0, 1.6]);
+set(hLeg, 'fontsize', 12);
 set(gca, 'fontsize', 12);
 xlabel("Time [s]");
 ylabel("Acceleration [m/s^2]");
