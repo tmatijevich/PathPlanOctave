02a78ab27f6e74ea13116a64f177c0fee889eeb9
Tyler Matijevich <tyler.matijevich@br-automation.com>
2021-03-24 14:14:50 -0500

    Additional comments and protection around a = 0.0

--- lib/GetAcc.m
+++ lib/GetAcc.m
@@ -36,7 +36,7 @@ function [Solution, Valid] = GetAcc(dt, dx, v0, vf, vmin, vmax, PrintResult = fa
 		Valid = false; 
 		return;
 	
-	% #3: Positive time and distance
+	% #3: Positive time duration and distance
 	elseif (dt <= 0.0) || (dx <= 0.0)
 		printf("GetAcc call failed: Time or distance non-positive %.3f, %.3f\n", dt, dx); 
 		Valid = false; 
@@ -50,42 +50,59 @@ function [Solution, Valid] = GetAcc(dt, dx, v0, vf, vmin, vmax, PrintResult = fa
 		
 	end % Requirements
 	
-	% v12 > v0, vf or v12 < v0, vf for a velocity profile with symmetric accel and decel
+	% The intermediate velocity point v12 is either >= v0, vf or <= v0, vf for a symmetric acc/dec profile
 	% Determine if 1. ACC 2. DEC or 1. DEC 2. ACC
 	NominalDistance = 0.5 * dt * (v0 + vf); % Area of a trapezoid
 	
 	if dx >= NominalDistance % 1. ACC 2. DEC
-		% Determine if saturated profile
+		% Determine if saturated
 		VmaxDistance = (2.0 * vmax ^ 2 - v0 ^ 2 - vf ^ 2) / (2.0 * ((2.0 * vmax - v0 - vf) / dt));
+		% NOTE: There is no dx >= NominalDistance when v0 = vf = vmax that also passes requirement #4. This protects against divide by zero.
 		
 		if dx < VmaxDistance % Acc/dec profile with peak
 			Solution.Move = PATH_ACC_DEC_PEAK;
 			
 		else % Acc/dec profile saturated at vmax
 			Solution.Move = PATH_ACC_DEC_SATURATED;
-			Solution.a = ((2.0 * vmax ^ 2 - v0 ^ 2 - vf ^ 2) / 2.0 - (2.0 * vmax - v0 - vf) * vmax) / (dx - dt * vmax);
-			Solution.v(2) = vmax;
-			Solution.v(3) = vmax;
-			Solution.t(2) = (vmax - v0) / Solution.a;
-			Solution.t(3) = dt - (vmax - vf) / Solution.a;
+			Solution.a = ((2.0 * vmax ^ 2 - v0 ^ 2 - vf ^ 2) / 2.0 - (2.0 * vmax - v0 - vf) * vmax) / (dx - dt * vmax); % Protected by requirement #4
+			if Solution.a > 0.0
+				Solution.v(2) = vmax;
+				Solution.v(3) = vmax;
+				Solution.t(2) = (vmax - v0) / Solution.a;
+				Solution.t(3) = dt - (vmax - vf) / Solution.a;
+			else
+				printf("GetAcc call warning: Unexpected non-positive acceleration\n"); % Should not happen
+				Solution.v(2) = v0;
+				Solution.v(3) = v0;
+				Solution.t(2) = 0.0;
+				Solution.t(3) = 0.0;
+			end % Positive acceleration
 			
 		end % VmaxDistance?
 		
 	else % 1. DEC 2. ACC
 		% Determine if saturated profile
-		VminDistance = (v0 ^ 2 + vf ^ 2 - 2.0 * vmin ^ 2) / (2.0 * ((v0 + vf - 2.0 * vmin) / dt));
+		VminDistance = (v0 ^ 2 + vf ^ 2 - 2.0 * vmin ^ 2) / (2.0 * ((v0 + vf - 2.0 * vmin) / dt))
+		% NOTE: There is no dx < NominalDistance when v0 = vf = vmin that also passes requirement #4. This protects against divide by zero.
 		
 		if dx > VminDistance % Dec/acc profile with dip
 			Solution.Move = PATH_DEC_ACC_PEAK;
 			
 		else % Dec/acc profile saturated at vmin
 			Solution.Move = PATH_DEC_ACC_SATURATED;
-			Solution.a = ((v0 ^ 2 + vf ^ 2 - 2.0 * vmin ^ 2) / 2.0 - (v0 + vf - 2.0 * vmin) * vmin) / (dx - dt * vmin);
-			Solution.v(2) = vmin;
-			Solution.v(3) = vmin;
-			Solution.t(2) = (v0 - vmin) / Solution.a;
-			Solution.t(3) = dt - (vf - vmin) / Solution.a;
-			
+			Solution.a = ((v0 ^ 2 + vf ^ 2 - 2.0 * vmin ^ 2) / 2.0 - (v0 + vf - 2.0 * vmin) * vmin) / (dx - dt * vmin); % Protected by requirement #4
+			if Solution.a > 0.0
+				Solution.v(2) = vmin;
+				Solution.v(3) = vmin;
+				Solution.t(2) = (v0 - vmin) / Solution.a;
+				Solution.t(3) = dt - (vf - vmin) / Solution.a;
+			else 
+				printf("GetAcc call warning: Unexpected non-positive acceleration\n"); % Should not happen
+				Solution.v(2) = v0;
+				Solution.v(3) = v0;
+				Solution.t(2) = 0.0;
+				Solution.t(3) = 0.0;
+			end % Positive acceleration
 		end % VminDistance?
 		
 	end % NominalDistance?
@@ -113,8 +130,13 @@ function [Solution, Valid] = GetAcc(dt, dx, v0, vf, vmin, vmax, PrintResult = fa
 			end
 			
 			Solution.a = abs(2.0 * Solution.v(2) - v0 - vf) / dt;
-			Solution.t(2) = abs(Solution.v(2) - v0) / Solution.a;
-			Solution.t(3) = Solution.t(2);
+			if Solution.a > 0.0
+				Solution.t(2) = abs(Solution.v(2) - v0) / Solution.a;
+				Solution.t(3) = Solution.t(2);
+			else % A flat line, dx = NominalDistance and v0 = vf
+				Solution.t(2) = 0.0;
+				Solution.t(3) = 0.0;
+			end
 			
 		end % Roots valid?
 	end % Peak movement?
