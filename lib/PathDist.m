d957a7391121fe61d3783a05289079a98a988798
Tyler Matijevich <tyler.matijevich@br-automation.com>
2021-06-02 17:55:59 -0500

    Update PathDist function definition to new naming convention

--- lib/PathDist.m
+++ lib/PathDist.m
@@ -1,6 +1,7 @@
 %!octave
 
-function [Solution, Valid] = GetDist(dt, v0, vf, vmin, vmax, a, PrintResult = false)
+function [solution, valid] = PathDist(dt, v_0, v_f, v_min, v_max, a, printResult = false)
+	% function [solution, valid] = PathDist(dt, v_0, v_f, v_min, v_max, a, printResult = false)
 	% Determine the maximum distance to change velocity with acceleration in time
 	% Assumptions:
 	% 	- Positive distance and velocity
@@ -15,80 +16,80 @@ function [Solution, Valid] = GetDist(dt, v0, vf, vmin, vmax, a, PrintResult = fa
 	global PATH_ACC_DEC_SATURATED;
 	
 	% Reset solution
-	Solution.t = [0.0, 0.0, 0.0, 0.0];
-	Solution.dx = 0.0;
-	Solution.v = [0.0, 0.0, 0.0, 0.0];
-	Solution.a = 0.0;
-	Solution.Move = PATH_MOVE_NONE;
+	solution.t = [0.0, 0.0, 0.0, 0.0];
+	solution.dx = 0.0;
+	solution.v = [0.0, 0.0, 0.0, 0.0];
+	solution.a = 0.0;
+	solution.move = PATH_MOVE_NONE;
 	
 	% Input requirements
 	% #1: Plausible velocity limits
-	if (vmin < 0.0) || (vmax <= vmin)
-		printf("GetDist call failed: Implausible velocity limits %1.3f, %1.3f\n", vmin, vmax); 
-		Valid = false; 
+	if (v_min < 0.0) || (v_max <= v_min)
+		printf("PathDist call failed: Implausible velocity limits %1.3f, %1.3f\n", v_min, v_max); 
+		valid = false; 
 		return;
 	
 	% #2: Endpoint velocities within limits
-	elseif (v0 < vmin) || (v0 > vmax) || (vf < vmin) || (vf > vmax)
-		printf("GetDist call failed: Endpoint velocities %1.3f, %1.3f exceed limits %1.3f, %1.3f\n", v0, vf, vmin, vmax); 
-		Valid = false; 
+	elseif (v_0 < v_min) || (v_0 > v_max) || (v_f < v_min) || (v_f > v_max)
+		printf("PathDist call failed: Endpoint velocities %1.3f, %1.3f exceed limits %1.3f, %1.3f\n", v_0, v_f, v_min, v_max); 
+		valid = false; 
 		return;
 	
 	% #3: Positive timespan and acceleration
 	elseif (dt <= 0.0) || (a <= 0.0)
-		printf("GetDist call failed: Timespan or acceleration non-positive %1.3f, %1.3f\n", dt, a); 
-		Valid = false; 
+		printf("PathDist call failed: Timespan or acceleration non-positive %1.3f, %1.3f\n", dt, a); 
+		valid = false; 
 		return;
 		
 	% #4: Valid timespan given acceleration
-	elseif dt < (abs(v0 - vf) / a)
-		printf("GetDist call failed: Impossible timepsan %1.3f given minimum %1.3f\n", dt, abs(v0 - vf) / a); 
-		Valid = false; 
+	elseif dt < (abs(v_0 - v_f) / a)
+		printf("PathDist call failed: Impossible timepsan %1.3f given minimum %1.3f\n", dt, abs(v_0 - v_f) / a); 
+		valid = false; 
 		return;
 		
 	end % Requirements
 	
 	% There is no distance advantage to decelerate then accelerate, therefore only ACC_DEC profiles will be considered
-	VmaxTime = (2 * vmax - v0 - vf) / a;
+	dt_u = (2 * v_max - v_0 - v_f) / a;
 	
 	% Check if saturated profile
-	if dt < VmaxTime % Acc/dec profile with peak
-		Solution.Move = PATH_ACC_DEC_PEAK;
+	if dt < dt_u % Acc/dec profile with peak
+		solution.move = PATH_ACC_DEC_PEAK;
 		
 		% Determine the peak velocity
-		vpeak = (dt * a + v0 + vf) / 2.0;
+		v_p = (dt * a + v_0 + v_f) / 2.0;
 		
 		% Set the solution
-		Solution.dx = (2.0 * vpeak ^ 2 - v0 ^ 2 - vf ^ 2) / (2.0 * a);
-		Solution.t(2) = (vpeak - v0) / a;
-		Solution.t(3) = (vpeak - v0) / a;
-		Solution.v(2) = vpeak;
-		Solution.v(3) = vpeak;
+		solution.dx = (2.0 * v_p ^ 2 - v_0 ^ 2 - v_f ^ 2) / (2.0 * a);
+		solution.t(2) = (v_p - v_0) / a;
+		solution.t(3) = (v_p - v_0) / a;
+		solution.v(2) = v_p;
+		solution.v(3) = v_p;
 		
-	else % Acc/dec profile saturated at vmax
-		Solution.Move = PATH_ACC_DEC_SATURATED;
+	else % Acc/dec profile saturated at v_max
+		solution.move = PATH_ACC_DEC_SATURATED;
 		
-		% Determine the distance at vmax
-		dx12 = (dt - (2.0 * vmax - v0 - vf) / a) * vmax;
+		% Determine the distance at v_max
+		dx_12 = (dt - (2.0 * v_max - v_0 - v_f) / a) * v_max;
 		
 		% Set the solution
-		Solution.dx = dx12 + (2.0 * vmax ^ 2 - v0 ^ 2 - vf ^ 2) / (2.0 * a);
-		Solution.t(2) = (vmax - v0) / a;
-		Solution.t(3) = (vmax - v0) / a + dx12 / vmax;
-		Solution.v(2) = vmax;
-		Solution.v(3) = vmax;
+		solution.dx = dx_12 + (2.0 * v_max ^ 2 - v_0 ^ 2 - v_f ^ 2) / (2.0 * a);
+		solution.t(2) = (v_max - v_0) / a;
+		solution.t(3) = (v_max - v_0) / a + dx_12 / v_max;
+		solution.v(2) = v_max;
+		solution.v(3) = v_max;
 		
 	end
 	
 	% Set common solution values and validate
-	Solution.t(4) = dt;
-	Solution.v(1) = v0;
-	Solution.v(4) = vf;
-	Solution.a = a;
-	Valid = true;
+	solution.t(4) = dt;
+	solution.v(1) = v_0;
+	solution.v(4) = v_f;
+	solution.a = a;
+	valid = true;
 	
-	if PrintResult
-		printf("GetDist call: Dist %.3f, Vel %.3f, Move %d\n", Solution.dx, Solution.v(2), Solution.Move);
+	if printResult
+		printf("PathDist call: Dist %.3f, Vel %.3f, Move %d\n", solution.dx, solution.v(2), solution.move);
 	end
 	
 end
