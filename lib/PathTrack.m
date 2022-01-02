4761332d9152cd2226d8b9f4d264cc8d1e187ac6
Tyler Matijevich <tyler.matijevich@br-automation.com>
2022-01-01 22:09:47 -0600

    Update path track and sample

--- lib/PathTrack.m
+++ lib/PathTrack.m
@@ -1,10 +1,10 @@
 %!octave
 
-%
-%
-%
+% 
+% 
+% 
 
-function [solution, valid] = PathTrack(x_0, v_0, t_s, x_1, v_max, a_max, printResult = false)
+function [solution, valid] = PathTrack(x_0, v_0, t_s, x_t, v_t, v_max, a_max, printResult = false)
     % Reference global variables
     run GlobalVars;
 
@@ -20,48 +20,41 @@ function [solution, valid] = PathTrack(x_0, v_0, t_s, x_1, v_max, a_max, printRe
     end
 
     % Check direction and initial velocity
-    if x_1 > x_0
+    if x_t > x_0
         dx_sign = 1.0; 
-    elseif x_1 < x_0
+    elseif x_t < x_0
         dx_sign = -1.0;
     else
         dx_sign = 0.0;
     end
-    if (dx_sign > 0.0 && v_0 < 0.0) || (dx_sign < 0.0 && v_0 > 0.0) || (dx_sign == 0.0 && v_0 != 0.0)
-        if abs(v_0) / t_s < a_max 
-            % Stop
-            solution.a = -1.0 * (v_0 / t_s);
-        else
-            % Decelerate
-            v_sign = v_0 / abs(v_0);
-            solution.a = (-1.0 * v_sign) * a_max; 
+    if (dx_sign > 0.0 && (v_0 < 0.0 || v_t < 0.0)) || (dx_sign < 0.0 && (v_0 > 0.0 || v_t > 0.0))
+        % Decelerate
+        [solution, valid] = PathTrackDec(x_0, v_0, t_s, a_max);
+        if printResult && valid
+            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f decelerate\n", x_t, solution.x, solution.v, solution.a);
         end
+        return;
+    elseif dx_sign == 0.0
+        % Match speed
+        dv_sign = (v_t - v_0) / abs(v_t - v_0);
+        solution.a = dv_sign * min(abs(v_t - v_0) / t_s, a_max);
         solution.v = v_0 + solution.a * t_s;
         solution.x = x_0 + v_0 * t_s + 0.5 * solution.a * t_s ^ 2;
-        valid = true;
         if printResult
-            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f decelerating\n", x_1, solution.x, solution.v, solution.a);
-            return;
+            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f match speed\n", x_t, solution.x, solution.v, solution.a);
         end
+        valid = true;
+        return;
     end
 
-    [timeSolution, timeValid] = PathTime(abs(x_1 - x_0), abs(v_0), 0.0, 0.0, v_max, a_max);
+    [timeSolution, timeValid] = PathTime(abs(x_t - x_0), abs(v_0), abs(v_t), 0.0, v_max, a_max);
     if timeValid == false
-        if abs(v_0) / t_s < a_max 
-            % Stop
-            solution.a = -1.0 * (v_0 / t_s);
-        else
-            % Decelerate
-            v_sign = v_0 / abs(v_0);
-            solution.a = (-1.0 * v_sign) * a_max; 
+        % Decelerate
+        [solution, valid] = PathTrackDec(x_0, v_0, t_s, a_max);
+        if printResult && valid
+            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f decelerate\n", x_t, solution.x, solution.v, solution.a);
         end
-        solution.v = v_0 + solution.a * t_s;
-        solution.x = x_0 + v_0 * t_s + 0.5 * solution.a * t_s ^ 2;
-        valid = true;
-        if printResult
-            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f decelerating\n", x_1, solution.x, solution.v, solution.a);
-            return;
-        end
+        return;
     elseif timeSolution.t(4) < t_s
         % Stop
         solution.a  = 0.0;
@@ -69,9 +62,9 @@ function [solution, valid] = PathTrack(x_0, v_0, t_s, x_1, v_max, a_max, printRe
         solution.x  = x_0 + v_0 * t_s;
         valid       = true;
         if printResult
-            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f within time-step %.3f\n", x_1, solution.x, solution.v, solution.a, t_s);
-            return;
+            printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f within time-step %.3f\n", x_t, solution.x, solution.v, solution.a, t_s);
         end
+        return;
     end
     [pointSolution, pointValid] = PathPoint(0.0, timeSolution.t, timeSolution.v, 4, t_s, 1.0);
     if pointValid == false 
@@ -84,7 +77,37 @@ function [solution, valid] = PathTrack(x_0, v_0, t_s, x_1, v_max, a_max, printRe
 
     valid = true;
     if printResult
-        printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f\n", x_1, solution.x, solution.v, solution.a);
+        printf("PathTrack call: xt %.3f, x %.3f, v %.3f, a %.3f\n", x_t, solution.x, solution.v, solution.a);
     end
+
+end % Function
+
+function [solution, valid] = PathTrackDec(x_0, v_0, t_s, a_max)
+
+    % Reset solution
+    solution    = struct('x', 0.0, 'v', 0.0, 'a', 0.0);
+    valid       = false;
+
+    % Input requirements
+    % #1 Positive inputs
+    if t_s <= 0.0 || a_max <= 0.0
+        printf("PathTrackDev call failed: Time step %.3f or max acceleration %.3f non-position\n", t_s, a_max);        
+        return;
+    end
+
+    if abs(v_0) / t_s < a_max
+        % Stop
+        solution.a = -1.0 * (v_0 / t_s);
+        solution.v = 0.0; % Force strictly zero
+        solution.x = x_0 + v_0 * t_s + 0.5 * solution.a * t_s ^ 2;
+    else
+        % Decelerate
+        v_sign = v_0 / abs(v_0);
+        solution.a = (-1.0 * v_sign) * a_max; 
+        solution.v = v_0 + solution.a * t_s;
+        solution.x = x_0 + v_0 * t_s + 0.5 * solution.a * t_s ^ 2;
+    end
+
+    valid = true;
 
 end % Function
